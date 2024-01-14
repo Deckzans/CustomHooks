205244f3f07bbc9e91e08dc0fd06351a1fa964e1
David Zamora <zdavid505@gmial.com>
2024-01-14 10:42:47 -0600

    Primer Commit

---
+++ useTodo.js
@@ -0,0 +1,54 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = []; 
+
+const init = () => { 
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+    const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
+    const todosCount = todos.length;
+    const pendingTodosCount = todos.filter(todo => !todo.done).length;
+
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+    
+    
+    const handleNewTodo = (todo) => { 
+        const action = { 
+            type: '[TODO] Add Todo',
+            payload: todo,
+        }
+        dispatchTodo(action);
+    }
+
+    const handleDeleteTodo = (id) => { 
+        // console.log(id)
+        dispatchTodo({ 
+            type: '[TODO] Remove Todo',
+            payload: id, 
+        })
+    }
+    const onToggleTodo = (id) => { 
+        console.log(id)
+        dispatchTodo({ 
+            type: '[TODO] Toggle Todo',
+            payload: id, 
+        })
+    }
+
+
+    return{ 
+        todos,
+        todosCount,
+        pendingTodosCount,
+        handleNewTodo,
+        handleDeleteTodo,
+        onToggleTodo,
+    }
+
+}
